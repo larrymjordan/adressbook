a296f4a5b0587bc199b444597dd701c8c1a03d57
Larry Morales Jordan <larrymoralesjordan@gmail.com>
2015-02-21 19:36:52 +0000

    [Feature] add users constraints validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :get_users, only: [:new, :edit]
 
   # GET /contacts
   # GET /contacts.json
@@ -70,5 +71,9 @@ class ContactsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
       params.require(:contact).permit(:name, :lastname, :phone, :email, :description, :birthday, :user_id)
+    end
+    
+    def get_users
+      @users = User.order('lastname').map {|user| ["#{user.name} #{user.lastname}", user.id]}
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
     has_many :contacts
+    validates :name, presence: true
 end
